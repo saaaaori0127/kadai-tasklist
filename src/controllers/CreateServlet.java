7c2eaa6a68dae40be7f9a7704b79df3d888fcde2
saori.nishi <sxoxr@i.softbank.jp>
2021-09-22 14:54:42 +0900

    EditServlet.javaを作成

--- src/controllers/CreateServlet.java
+++ src/controllers/CreateServlet.java
@@ -36,16 +36,16 @@ public class CreateServlet extends HttpServlet {
             EntityManager em = DBUtil.createEntityManager();
             em.getTransaction().begin();
 
-            Task m = new Task();
+            Task t = new Task();
 
             String content = request.getParameter("content");
-            m.setContent(content);
+            t.setContent(content);
 
             Timestamp currentTime = new Timestamp(System.currentTimeMillis());
-            m.setCreated_at(currentTime);
-            m.setUpdated_at(currentTime);
+            t.setCreated_at(currentTime);
+            t.setUpdated_at(currentTime);
 
-            em.persist(m);
+            em.persist(t);
             em.getTransaction().commit();
             em.close();
 
